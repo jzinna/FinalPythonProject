2e875231de9d9fbba0edb537ab3e2bfa9e097a8f
jzinna <jzinna@gmail.com>
2017-06-03 00:49:54 -0700

    Update txt_analyzer.py

--- txt_analyzer.py
+++ txt_analyzer.py
@@ -1,17 +1,22 @@
-"""
-#>>> main('caesar.txt')
-The average word length is 5
+'''
+Write a text analyzer. It should be in a form of a function that takes a file name as an argument. It should read and
+analyze a text file and then print:
+- the top 5 most frequently used words in the file
+- the number of times the top 5 words are used
+- should be sorted by most frequently used count
+- the longest word in the document
+- the average size of each word'''
 
-"""
 
 def main(file_name):
     txt_file_object = open(file_name, 'r')
-    all_text_lstOfLines = txt_file_object.readlines()   # takes the whole text and creates a list where each position is a line
+    all_text_lstOfLines = txt_file_object.readlines()   # assuming text is short enough to avoid memory problems, takes the whole text and creates a list where each position is a line
     data = get_data(all_text_lstOfLines)
+    print('The five most common words and the number of times they occur are %s' % (data[0]))
+    print('The longest word is \"%s\"' % (data[2]))
     print('The average word length is %5.3f' % (data[1]))   # data[1] is average_size
-    print('Word usage is as follows %s' % (data[0]))        # data[0] is word_count_dic
-    #five_most_used(data[0])
-    print(data[2])
+
+
 
 
 def get_data(all_text_lstOfLines):
@@ -28,8 +33,9 @@ def get_data(all_text_lstOfLines):
             if len(word) > len(longest_word):
                 longest_word = word
             most_used_words(word,word_count_dic)
+    top_five = five_most_used(word_count_dic)
     average_size =  total_size/count
-    return word_count_dic, average_size, longest_word
+    return top_five, average_size, longest_word
 
 
 def most_used_words(word,word_count_dic):
@@ -40,23 +46,12 @@ def most_used_words(word,word_count_dic):
         word_count_dic[word] = 1
 
 def five_most_used(dic):
-    top_five = {}
-    for x,y in dic.items():
-        pass
-
-# Assuming that file is small enough to read in completely and not need to read line by line
-main('test.txt')
+    word_number_pairs = list(dic.items())
+    ordered_most_common = sorted(word_number_pairs, key=lambda word_number_pairs: word_number_pairs[1], reverse=True)
+    ordered_five_most_common = ordered_most_common[:5]
+    return ordered_five_most_common
 
 
-'''
-Write a text analyzer. It should be in a form of a function that takes a file name as an argument. It should read and 
-analyze a text file and then print:
-- the top 5 most frequently used words in the file
-- the number of times the top 5 words are used
-- should be sorted by most frequently used count
-- the longest word in the document
-- the average size of each word'''
 
-if __name__== "__main__":
-    import doctest
-    doctest.testmod()
+
+main('macbeth.txt')
